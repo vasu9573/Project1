8a1dd92ae7a7b1f4fcbf544fce9dd676c02abe8d
vasantha S <vasanthaseera13@gmail.com>
2023-11-23 12:27:06 +0530

    version 1

---
+++ emicalculator.py
@@ -0,0 +1,11 @@
+from customer import Customer
+from emi import EMI
+
+cust1=Customer('Raju',100000,12)
+eobj=EMI()
+eobj.emicalculator(cust1)
+
+
+cust2=Customer('vasu',150000,18)
+eobj=EMI()
+eobj.emicalculator(cust2)
